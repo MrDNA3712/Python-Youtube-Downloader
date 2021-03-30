31f19dac58d4a1c59e5effab7b0872f26c50f6eb
MrDNA <jr.andreesen@gmail.com>
2021-03-30 16:29:44 +0200

    options for output filename

--- videodownload.py
+++ videodownload.py
@@ -4,15 +4,11 @@ import json
 from tqdm import tqdm
 import argparse
 
-test_id = "ucbx9we6EHk"
-
-def getLinks(id):
+def get_video_info(id):
     video_info_link="https://www.youtube.com/get_video_info?video_id={}".format(id)
     video_info=requests.get(video_info_link)
     response = parse.parse_qs(video_info.text)
-    pr = json.loads(response['player_response'][0])
-    formats = pr['streamingData']['formats']
-    return formats
+    return json.loads(response['player_response'][0])
 
 def download(video_url,filename='download.mp4'):
     print(filename)
@@ -29,12 +25,13 @@ def download(video_url,filename='download.mp4'):
 if __name__=='__main__':
     parser = argparse.ArgumentParser(prog="Python Youtube Downloader", description="Simple Youtube Downloader")
     parser.add_argument("video_id", help="The youtube video id",nargs=1)
-    parser.add_argument("video_filename", help="Where the downloaded video will be saved",nargs='?', default='download.mp4')
+    parser.add_argument("video_filename", help="Where the downloaded video will be saved",nargs='?', default='')
     parser.add_argument("--chooseQuality", "-cq", help="Display all available video qualities and choose which to download", action='store_const',const=True,default=False)
     args = parser.parse_args()
 
-    options = getLinks(args.video_id[0])
+    video_info = get_video_info(args.video_id[0])
 
+    options = video_info['streamingData']['formats']
     if args.chooseQuality:
         num = 1
         for option in options:
@@ -49,7 +46,9 @@ if __name__=='__main__':
         auswahl = -1
 
     video_url = options[auswahl]['url']
-    if not args.video_filename[-4:] == '.mp4':
+    if args.video_filename == '':
+        outfile = video_info['videoDetails']['title'] + '.mp4'
+    elif len(args.video_filename) < 4 or not args.video_filename[-4:] == '.mp4':
         outfile=args.video_filename + '.mp4'
     else:
         outfile=args.video_filename
